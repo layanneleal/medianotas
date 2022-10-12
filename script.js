0354a21a995bc46e4d00106584bb4b9e742b5de9
Layanne Bezerra Leal <layanneliz@gmail.com>
2022-10-12 08:34:15 -0300

    Update

--- script.js
+++ script.js
@@ -1,26 +1,29 @@
 function mediaNotas() {
+
   let saida = document.querySelector('p#res')
   let nota1 = prompt('Digite a primeira nota')
   let nota2 = prompt('Digite a segunda nota')
   let qtNotas = 2
-  let media 
-  let soma
+  let media = 0
+  let soma = 0
   
   nota1 = nota1.replace('.', ',')
   nota2 = nota2.replace('.', ',')
 
   nota1 = parseFloat(nota1)
   nota2 = parseFloat(nota2)
+  //alert(nota1 +' - '+nota2)
   
   if (nota1 > 0 && nota2 > 0 ) {
 
     soma = nota1 + nota2
     media = soma / qtNotas
+    //alert(media)
 
     saida.innerHTML = `<p>Resultado:</p>`
-    saida.innerHTML =+ `<p>Primeira nota ${nota1}</p>`
-    saida.innerHTML =+ `<p>Segunda nota ${nota2}</p>`
-    saida.innerHTML =+ `<p>Média: ${media}</p>`
+    saida.innerHTML += `<p>Primeira nota ${nota1}</p>`
+    saida.innerHTML += `<p>Segunda nota ${nota2}</p>`
+    saida.innerHTML += `<p>Média: ${media}</p>`
 
   }else{
     alert('Digite todas as notas')
